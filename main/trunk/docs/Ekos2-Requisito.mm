1ae390fd6f0ae1c29467eef0f878d37ae747f00b
kelson <kelson@93951b0e-86a9-4c03-91bb-e41827ad4496>
2009-12-01 15:03:14 +0000

    Separado em arquivos.

---
+++ main/trunk/docs/Ekos2-Requisito.mm
@@ -0,0 +1,224 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1256311660468" ID="Freemind_Link_1739200689" MODIFIED="1257950709406" TEXT="Ekos2">
+<node CREATED="1259338851109" ID="Freemind_Link_1646671729" MODIFIED="1259338943206" POSITION="right" TEXT="Pesquisa e recursos">
+<icon BUILTIN="xmag"/>
+</node>
+<node CREATED="1257279403593" ID="Freemind_Link_903543847" MODIFIED="1257362746625" POSITION="right" TEXT="Applicaton">
+<node CREATED="1257279403593" ID="Freemind_Link_1414775589" MODIFIED="1257279403593" TEXT="File manager"/>
+<node CREATED="1257279403593" ID="Freemind_Link_1036895562" MODIFIED="1257279403593" TEXT="Microblog"/>
+<node CREATED="1257279403593" MODIFIED="1257279403593" TEXT="Note"/>
+<node CREATED="1257362871828" ID="Freemind_Link_1473105342" MODIFIED="1257362878578" TEXT="Integration">
+<node CREATED="1257362879562" ID="Freemind_Link_1855024747" MODIFIED="1257362882140" TEXT="Wiki"/>
+<node CREATED="1257362883218" ID="Freemind_Link_155994980" MODIFIED="1257362886156" TEXT="Moodle"/>
+<node CREATED="1257279403593" MODIFIED="1257279403593" TEXT="Calendar"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_105859885" MODIFIED="1257950386140" TEXT="IDM">
+<icon BUILTIN="full-1"/>
+<node CREATED="1257362624453" ID="Freemind_Link_305087807" MODIFIED="1257950432765" TEXT="Disable">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_1047084449" MODIFIED="1257950406125" TEXT="Single Sign-On / Single Sign-Out">
+<icon BUILTIN="full-1"/>
+<node CREATED="1256311665625" MODIFIED="1256311665625" TEXT="OpenId"/>
+<node CREATED="1256311665625" ID="Freemind_Link_494094166" MODIFIED="1256311665625" TEXT="SAML"/>
+</node>
+<node CREATED="1257950396046" ID="Freemind_Link_279206216" MODIFIED="1257950406125" TEXT="Self-Service">
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1257950421312" ID="Freemind_Link_1080384937" MODIFIED="1257950432765" TEXT="IDA">
+<node CREATED="1257279004000" ID="Freemind_Link_73694016" MODIFIED="1257362607296" TEXT="CRUD"/>
+<node CREATED="1257362620375" ID="Freemind_Link_79439974" MODIFIED="1257362624062" TEXT="Bulk"/>
+<node CREATED="1257950435375" ID="Freemind_Link_548606793" MODIFIED="1257950437437" TEXT="Disable"/>
+</node>
+<node CREATED="1257279403593" ID="Freemind_Link_1399438812" MODIFIED="1257279403593" TEXT="Future">
+<node CREATED="1257279403593" MODIFIED="1257279403593" TEXT="Issue Tracker"/>
+<node CREATED="1257279403593" MODIFIED="1257279403593" TEXT="News reader"/>
+<node CREATED="1257279403593" MODIFIED="1257279403593" TEXT="Project Manager"/>
+</node>
+</node>
+<node CREATED="1256311665609" ID="Freemind_Link_1940496041" MODIFIED="1257950618625" POSITION="left" TEXT="Features">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665609" ID="Freemind_Link_1140395207" MODIFIED="1256838254093" TEXT="Tag">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_84588110" MODIFIED="1256838254093" TEXT="Node">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1650105099" MODIFIED="1256570634500" TEXT="Localization">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_274654982" MODIFIED="1257950582703" TEXT="Search">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_1727664118" MODIFIED="1257950596781" TEXT="Specific">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_921339795" MODIFIED="1257950596765" TEXT="General">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1765402980" MODIFIED="1257950607921" TEXT="Notifier">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1415780629" MODIFIED="1257950611671" TEXT="Theme">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_974947493" MODIFIED="1257950582703" TEXT="SSO">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1083941699" MODIFIED="1257278808421" TEXT="Avatar">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_847904401" MODIFIED="1257362736890" TEXT="Increment">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665609" ID="Freemind_Link_939096925" MODIFIED="1257362735640" TEXT="Content Fields">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_1852635118" MODIFIED="1256838190375" TEXT="Title">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1401715944" MODIFIED="1256838190375" TEXT="URI">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_728921946" MODIFIED="1256838190375" TEXT="Tags">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_975281816" MODIFIED="1256838190375" TEXT="Authors">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_319892912" MODIFIED="1256838190375" TEXT="Text content">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_187017454" MODIFIED="1256838179312" TEXT="Date">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_654447532" MODIFIED="1256838170312" TEXT="Creation">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_972355836" MODIFIED="1256838173609" TEXT="Last change">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_993518605" MODIFIED="1259339008896" POSITION="right" TEXT="Service">
+<icon BUILTIN="xmag"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1257279004000" ID="Freemind_Link_1827295500" MODIFIED="1259339012664" TEXT="Conference">
+<icon BUILTIN="xmag"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_29922778" MODIFIED="1259338974223" TEXT="Instant Message">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_145831377" MODIFIED="1259338989767" TEXT="Notification">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_499216940" MODIFIED="1259338994639" TEXT="Search">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1925665369" MODIFIED="1257950503890" POSITION="left" TEXT="Design Project">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_1496998132" MODIFIED="1256837913937" TEXT="DB Agnostic">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_1742144572" MODIFIED="1256837881187" TEXT="Persistence">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_933236131" MODIFIED="1256837836734" TEXT="Doctrine">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_118473201" MODIFIED="1256837873953" TEXT="Zend">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1289773435" MODIFIED="1256837831765" TEXT="DB Abstraction">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_890187709" MODIFIED="1256837826281" TEXT="PDO">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_901872765" MODIFIED="1256837614187" TEXT="ADOdb">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1317154039" MODIFIED="1256837616750" TEXT="PEAR_DB">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1097742493" MODIFIED="1256837823656" TEXT="ZEND_DB">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node CREATED="1256317748125" ID="Freemind_Link_1466067066" MODIFIED="1256838213484" TEXT="Dialects">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_119321808" MODIFIED="1256838217656" TEXT="PostgreSQL">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1155037001" MODIFIED="1256838217656" TEXT="MySQL">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_108411820" MODIFIED="1257950495578" TEXT="Internacionalization">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_550644070" MODIFIED="1256312096796" TEXT="XML">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1755261766" MODIFIED="1256312808953" TEXT="Locale changer">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_308276791" MODIFIED="1256312035015" TEXT="PureMVC">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256312286062" ID="Freemind_Link_992757579" MODIFIED="1256570600171" TEXT="WebService">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256311665625" ID="Freemind_Link_286317141" MODIFIED="1256312037656" TEXT="amfphp">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256312077703" ID="Freemind_Link_986807323" MODIFIED="1256312098593" TEXT="JSON">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256312083375" ID="Freemind_Link_1596675109" MODIFIED="1256312100828" TEXT="AMF">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1849914394" MODIFIED="1256570591140" TEXT="Server Framework">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1256312263156" ID="Freemind_Link_1182593863" MODIFIED="1256837859250" TEXT="None">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1978831392" MODIFIED="1256837864515" TEXT="Zend">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_354723115" MODIFIED="1256312619781" TEXT="Symfony">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_649111310" MODIFIED="1256312621968" TEXT="CodeIgniter">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1256311665625" ID="Freemind_Link_1555162971" MODIFIED="1256312624625" TEXT="CakePHP">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_1107713452" MODIFIED="1259339087802" POSITION="right" TEXT="Design">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="xmag"/>
+<node CREATED="1257279004000" ID="Freemind_Link_1995338235" MODIFIED="1257950464546" TEXT="Cockpit">
+<icon BUILTIN="full-1"/>
+<node CREATED="1257279004000" ID="Freemind_Link_1608971515" MODIFIED="1257279004000" TEXT="Widget"/>
+<node CREATED="1257362648343" ID="Freemind_Link_1230552353" MODIFIED="1257950464546" TEXT="Perspective">
+<icon BUILTIN="full-1"/>
+<node CREATED="1257279004000" ID="Freemind_Link_1135537543" MODIFIED="1257950456000" TEXT="General">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_661584906" MODIFIED="1257279004000" TEXT="Manager"/>
+<node CREATED="1257279004000" ID="Freemind_Link_1894071658" MODIFIED="1257279004000" TEXT="Administrator"/>
+</node>
+</node>
+<node CREATED="1257279004000" ID="Freemind_Link_1073270226" MODIFIED="1257950656093" TEXT="Theme">
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+</node>
+</map>
